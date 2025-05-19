348319f1686bb1fcb4bfe6a8e398733c13adc2d2
HoussamBoukdir <boukdir.houssam166@gmail.com>
2025-05-19 14:36:34 +0100

    Add files via upload

---
+++ POO.py
@@ -0,0 +1,17 @@
+class Etudiant:
+    # attributs
+    nom = ''
+    age= 0
+
+    # instanciation
+
+    etd1 = Etudiant ()
+    etd1.nom = "ali"
+    etd.age = 27
+    etd2 = Etudiant ()
+    etd.nom = "amine"
+    etd2.age = 19
+    # affichage
+    
+    print(etd1.nom)
+    print(etd2.age)
